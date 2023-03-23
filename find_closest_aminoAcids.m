223fdaf59619e416410544d89c99da39c7cf9a0a
yeela8g <118124478+yeela8g@users.noreply.github.com>
2023-03-23 15:07:54 +0200

    Update find_closest_aminoAcids.m

--- find_closest_aminoAcids.m
+++ find_closest_aminoAcids.m
@@ -3,16 +3,9 @@ function index_AA = find_closest_aminoAcids(user_info, dist_matrix, min_dist)
 %atoms and ligand atom, and a constat distance and return all the amino
 %acid identifiers that are in the ligand binding site.
 
-% initialize output array
-out = false(1, size(dist_matrix, 2));
-
-% iterate over columns of the matrix
-for i = 1:size(dist_matrix, 2)
-    % compare each value in the column to the minimum distance
-    col = dist_matrix(:, i) < min_dist;
-    % use any command to find if there is at least one element that is true
-    out(i) = any(col);
-end
+% Find in which columns there are 2 amino acids that have a shorter
+% distance than min_dist.
+out = any(dist_matrix < min_dist,1);
 
 %retrieve all the protein atoms that are close enough to at least one ligand atom
 relevant_protein_atoms = user_info.protein_atoms(out);
